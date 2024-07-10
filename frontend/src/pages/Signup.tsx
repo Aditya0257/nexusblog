24910ccd01be9f98b9208769722b7220b21d458e
Aditya0257 <adityasinghthakur.0257@gmail.com>
2024-07-11 00:02:53 +0530

    some page changes in fe

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,11 @@
+import { useRecoilValue } from "recoil";
 import { Auth } from "../components/Auth";
 import { Quote } from "../components/Quote";
+import { userAtom } from "../store/atoms/user";
 
 export const Signup = () => {
+  const { username, isAuthenticated } = useRecoilValue(userAtom);
+
   return (
     <div>
       <div className="grid grid-cols-1 lg:grid-cols-2">
